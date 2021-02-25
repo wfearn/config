a2e7b6311af9071a97e8351c727b507a00c16e87
Wilson Fearn <wilson.fearn@gmail.com>
2021-02-25 12:29:15 -0800

    Included .cs snippet in setup and made personal note

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@ mkdir -p ~/.vim/snippets
 mkdir -p ~/.vim/bundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
+# TODO: Make this grab all snippet files instead of having to input these individually
 # terminal
 ln -s $REPO_HOME/.inputrc $HOME/.inputrc
 ln -s $REPO_HOME/vim/.vimrc $HOME/.vimrc
@@ -13,6 +14,7 @@ ln -s $REPO_HOME/vim/snippets/python.snippets $HOME/.vim/snippets/python.snippet
 ln -s $REPO_HOME/vim/snippets/bib.snippets $HOME/.vim/snippets/bib.snippets
 ln -s $REPO_HOME/vim/snippets/java.snippets $HOME/.vim/snippets/java.snippets
 ln -s $REPO_HOME/vim/snippets/html.snippets $HOME/.vim/snippets/html.snippets
+ln -s $REPO_HOME/vim/snippets/cs.snippets $HOME/.vim/snippets/cs.snippets
 
 vim +PluginInstall +qa
 
